af7999ef71b61fdd0e125e8b82f264add604a06d
abdelrahman-tarek-0 <y.p.challenges@gmail.com>
2024-03-24 14:37:22 +0200

    Refactor captchaCheck function to handle errors

--- src/utils/captchaCheck.util.js
+++ src/utils/captchaCheck.util.js
@@ -1,21 +1,22 @@
 import axios from 'axios'
-import { ErrorMessage } from './ErrorMessage'
 
 const captchaCheck = async (captcha) => {
-   const secret = process.env.CLOUDFLARE_CAPTCHA_KEY
-   if (!captcha) return false
-
-   const response = await axios.post(
-      'https://challenges.cloudflare.com/turnstile/v0/siteverify',
-      {
-         secret,
-         response: captcha,
-      }
-   )
-
-   if (response.data.success) return true
-
-   return false
+   try {
+      const secret = process.env.CLOUDFLARE_CAPTCHA_KEY
+      if (!captcha) return false
+      const response = await axios.post(
+         'https://challenges.cloudflare.com/turnstile/v0/siteverify',
+         {
+            secret,
+            response: captcha,
+         }
+      )
+      if (response.data.success) return true
+      return false
+   } catch (error) {
+      console.error(error)
+      return false
+   }
 }
 
 export default captchaCheck
